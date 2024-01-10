581a5af5a96dd56fd99a9db26b54e75953967be9
Zacharia Mwaura <75284078+ZachariaMwaura@users.noreply.github.com>
2024-01-10 15:35:02 -0500

    Initial Commit

---
+++ Quiz App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by Zach Mwaura on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
